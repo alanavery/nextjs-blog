fd89126e59d829fa8f7bf4bf5b4116c0960bb17a
Alan Avery <delayedaa@gmail.com>
2022-10-10 14:20:00 -0500

    add google analytics scripts

--- pages/posts/first-post.jsx
+++ pages/posts/first-post.jsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Script from 'next/script';
 import Link from 'next/link';
 import Image from 'next/image';
 
@@ -8,7 +9,23 @@ const FirstPost = () => {
       <Head>
         <title>First Post</title>
       </Head>
-      
+
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-FYJ4SB5KTJ"
+        strategy="lazyOnload"
+        onLoad={() => {
+          console.log('Script loaded correctly: googletagmanager.com');
+        }}
+      />
+      <Script
+        src="/scripts/google-tag.js"
+        strategy="lazyOnload"
+        onLoad={() => {
+          console.log('Script loaded correctly: google-tag.js');
+        }}
+      />
+
       <h1>First Post</h1>
       <h2>
         <Link href="/">
---
+++ public/scripts/google-tag.js
@@ -0,0 +1,8 @@
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+  dataLayer.push(arguments);
+}
+
+gtag('js', new Date());
+gtag('config', 'G-FYJ4SB5KTJ');
