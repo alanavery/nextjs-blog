8a0a096b9e02724d425fab3a3ac7449b1615247f
Alan Avery <delayedaa@gmail.com>
2022-10-10 17:16:30 -0500

    complete assets, metadata, and css lesson

--- pages/posts/first-post.jsx
+++ pages/posts/first-post.jsx
@@ -2,10 +2,11 @@ import Head from 'next/head';
 import Script from 'next/script';
 import Link from 'next/link';
 import Image from 'next/image';
+import Layout from '../../components/layout';
 
 const FirstPost = () => {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
       </Head>
@@ -27,12 +28,7 @@ const FirstPost = () => {
       />
 
       <h1>First Post</h1>
-      <h2>
-        <Link href="/">
-          <a className="post__link">Back to home</a>
-        </Link>
-      </h2>
-    </>
+    </Layout>
   );
 };
 
